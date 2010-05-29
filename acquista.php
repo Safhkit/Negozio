6651a8e080b4ce8f978a8498964432f1510fc446
vince.pii <vinc.pii@gmail.com>
2010-05-29 02:00:27 +0200

    Preparazione pagamento

--- acquista.php
+++ acquista.php
@@ -1,3 +1,26 @@
+<?php
+function formConferma($id, $num, $l)
+{
+	$query = "SELECT * FROM negozio.prodotti
+						WHERE id =" .$id. ";";
+
+	$result = mysql_query($query, $l);
+	if (!$result)
+		die ('Invalid query: ' . mysql_error());
+	$row = mysql_fetch_assoc($result);
+		
+	echo "Riepilogo: <br />";
+	echo "Prodotto: ".$row['nome']."<br />";
+	echo "Quantità: ".$num."<br />";
+	echo "Prezzo totale: ".$row['prezzo']." x " .$num. " = " .$row['prezzo'] * $num ."<br />";
+	echo "<form name=\"Conferma\" action=\"pagamento.php\" method=\"get\" >";
+	echo "<input type=\"hidden\" name=\"id\" value=\"".$id."\" />";
+	echo "<input type=\"hidden\" name=\"num\" value=\"".$num."\" />";
+	echo "<input type=\"submit\" value=\"Conferma Pagamento\" />";
+	echo "</form>";
+}
+?>
+
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
 "http://www.w3.org/TR/html4/loose.dtd">
 <html>
@@ -14,8 +37,7 @@
 		$user = 'user';
 		$password = 'password';
 		$link = mysql_connect('localhost', $user, $password);
-		
-		//cancellazione entry scadute		
+				
 		//lock su prenotazioni perché un altro utente potrebbe leggere le stesse entry da
 		//cancellare e ci potrebbero essere corse sulla cancellazione, su prodotti
 		//perché si modifica la disponibilità e non si vuole che qualche utente
@@ -25,37 +47,73 @@
 		if (!$result)
 			die ('Invalid query: ' . mysql_error());
 		
+		//cancellazione entry scadute e ripristino delle disponibilità in prodotti
 		$now = time();
 		$query = "update negozio.prodotti, (
-							select prod_id, sum(pezzi) as somma
-							from negozio.prenotazioni
-							where scadenza < FROM_UNIXTIME(".$now.")
-							group by prod_id ) as T
-							set negozio.prodotti.disponibili = negozio.prodotti.disponibili + T.somma
-							where negozio.prodotti.id = T.prod_id;";
+				select prod_id, sum(pezzi) as somma
+				from negozio.prenotazioni
+				where scadenza < FROM_UNIXTIME(".$now.")
+				group by prod_id ) as T
+				set negozio.prodotti.disponibili = negozio.prodotti.disponibili + T.somma
+				where negozio.prodotti.id = T.prod_id;";
 		$result = mysql_query($query, $link);
 		if (!$result)
 			die ('Invalid query: ' . mysql_error());
 			
 		$query = "DELETE FROM negozio.prenotazioni 
-				  WHERE scadenza < FROM_UNIXTIME(".$now.");";
+				WHERE scadenza < FROM_UNIXTIME(".$now.");";
 		$result = mysql_query($query, $link);
 		if (!$result)
 			die ('Invalid query: ' . mysql_error());
 			
 		$query = "SELECT disponibili FROM negozio.prodotti
-							WHERE id =". $id;
-		//TODO: mettere in una funzione del tipo fai_query($query, $link) e ritorna $result
+				WHERE id =". $id;
 		$result = mysql_query($query, $link);
 		if (!$result)
 			die ('Invalid query: ' . mysql_error());
 		
 		$row = mysql_fetch_assoc($result);
+		
+		//la disponiblità vista dall'utente potrebbe essere cambiata
 		if ($num <= $row["disponibili"]) {
-			//TODO: proseguire da qui
-			//inserimento in prenotazioni
-			//per l'inserimento in scadenza: DATE_ADD(NOW(), INTERVAL 1 HOUR)
-			//modifica in prodotti
+			//se la prenotazione esiste già, si incrementa pezzi
+			//altrimenti si inserisce la nuova prenotazione.
+			//TODO: mettere nome utente invece di XXX
+			$query = "SELECT * FROM negozio.prenotazioni
+					where user_id = 'XXX' and prod_id = ".$id.";";
+			$result = mysql_query($query, $link);
+			if (!$result)
+				die ('Invalid query: ' . mysql_error());
+			
+			$row = mysql_fetch_assoc($result);
+			if (!$row) {
+				//prenotazione non esisteva
+				//TODO: mettere nome utente invece di XXX
+				$query = "INSERT INTO negozio.prenotazioni
+					values(".$id.", 'XXX', ".$num.", DATE_ADD(NOW(), INTERVAL 1 HOUR));";
+				$result = mysql_query($query, $link);
+				if (!$result)
+					die ('Invalid query: ' . mysql_error());
+			}
+			else {
+				//aggiungere alla prenotazione e rinnovare la scadenza
+				//TODO: mettere nome utente invece di XXX
+				$query = "UPDATE negozio.prenotazioni
+						SET pezzi = pezzi +".$num.", scadenza = DATE_ADD(NOW(), INTERVAL 1 HOUR)
+						WHERE prod_id = ".$id." and user_id = 'XXX';";
+				$result = mysql_query($query, $link);
+				if (!$result)
+					die ('Invalid query: ' . mysql_error());
+			}
+			
+			$query = "update negozio.prodotti
+					set negozio.prodotti.disponibili = negozio.prodotti.disponibili - ".$num."
+					where negozio.prodotti.id = ".$id.";";
+			$result = mysql_query($query, $link);
+			if (!$result)
+				die ('Invalid query: ' . mysql_error());
+			
+			formConferma($id, $num, $link);
 		}
 		else {
 			$query = "UNLOCK TABLES;";
@@ -69,8 +127,6 @@
 		$result = mysql_query($query, $link);
 		if (!$result)
 			die ('Invalid query: ' . mysql_error());
-
-		//transazione con la banca
 		?>
 		</div>
 	</body>
